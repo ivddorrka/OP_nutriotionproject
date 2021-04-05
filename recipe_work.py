12cc5876524023b7549e936a00d7e0459561aff4
ivddorrka <ivddorrka@gmail.com>
2021-04-05 06:54:52 +0300

    Rec v2

--- recipe_work.py
+++ recipe_work.py
@@ -4,7 +4,7 @@ This is what we need from *.json recipes files
 import json 
 import random 
 
-f = open('recipes_raw_nosource_ar.json')
+f = open('recipes/recipes_raw_nosource_ar.json')
 data = json.load(f)
 random_recipe_id = random.choice(list(data))
 info = data[random_recipe_id]
