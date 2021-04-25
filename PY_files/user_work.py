5d22e66fbecd7897bec386102e1bb79c38b7053f
ivddorrka <ivddorrka@gmail.com>
2021-04-25 20:35:33 +0300

    User work example

--- PY_files/user_work.py
+++ PY_files/user_work.py
@@ -2,38 +2,42 @@
 
 class User:
     """To get info from user"""
-    def __init__(self, age, height, weight, gender, activity):
+    def __init__(self, login, password):
         """Init function"""
-        self.age = age
-        self.height = height
-        self.weight = weight
-        self.gender = gender
-        self.activity = activity
+        self.age = None
+        self.height = None
+        self.weight = None
+        self.gender = None
+        self.activity = None
+        self.login = login
+        self.password = password
 
-    def set_characteristics(self):
+    def set_characteristics(self, age, height, weight, gender, act):
         """To set all characteristics"""
-        self.get_age()
-        self.get_height()
-        self.get_weight()
+        self.get_age(age)
+        self.get_height(height)
+        self.get_weight(weight)
+        self.get_activ(act)
 
-    def get_age(self):
+
+    def get_age(self, age):
         """To get age"""
-        self.age = int(self.age)
+        self.age = int(age)
         
-    def get_height(self):
+    def get_height(self, height):
         """To get height"""
-        self.height = int(self.height)
+        self.height = int(height)
 
 
-    def get_weight(self):
+    def get_weight(self, weight):
         """To get weight"""
-        self.height = int(self.height)
+        self.weight = int(weight)
     
-    def get_activ(self):
+    def get_activ(self, act):
         """To get a number of activity"""
-        self.activity = float(self.activity.split()[0])
+        self.activity = float(act.split()[0])
         
             
-if __name__=="__main__":
-    us = User()
-    us.set_characteristics()
+# if __name__=="__main__":
+#     us = User()
+#     us.set_characteristics()
--- app_work.py
+++ app_work.py
@@ -8,30 +8,53 @@ def home():
     """
     Home page
     """
-    return render_template("index.html")
+    return render_template("registration.html")
 
-@app.route("/questions", methods=["POST"])
-def register():
+# @app.route("/registration", methods=["POST"])
+# def register():
+#     """
+#     To get data from user
+#     """
+#     login = request.form.get("login")
+#     password = request.form.get("password")
+#     # redirect(url_for('questions'))
+#     return user_work.User(login, password)
+
+
+
+@app.route("/registration", methods=["POST"])
+def infor_user():
     """
     To get data from user
     """
+    # render_template("index.html")
+    login = request.form.get("login")
+    password = request.form.get("password")    
     age = request.form.get("age")
     height = request.form.get("height")
     weight = request.form.get("weight")
     gender = request.form.get("gender")
     act = request.form.get("comp_select")
-    user = user_work.User(age,height, weight, gender,act)
-    return user
+    user = user_work.User(login, password)
+    return user, age, height, weight, gender, act
 
 
-@app.route("/questions/submitted", methods=["POST"])
+@app.route("/registration/submitted", methods=["POST"])
 def file_html():
     """
     To return already submitted page
     """
-    user = register()
+    # user = register()
+    info = infor_user()
+    user = info[0]
+    age = info[1]
+    height = info[2]
+    weight = info[3]
+    gender = info[4]
+    act = info[5]
+
     try:
-        user.set_characteristics()
+        user.set_characteristics(age, height, weight, gender, act)
         return render_template("calc.html")
     except ValueError:
         return render_template("failure.html")
