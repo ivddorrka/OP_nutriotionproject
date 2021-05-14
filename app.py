f335f2e2dc65836a12579ef2a0ce85294aaa5e57
archy-co <yaroslav.moskalyk@ucu.edu.ua>
2021-05-15 00:34:19 +0300

    Home page and app route

--- app.py
+++ app.py
@@ -13,6 +13,20 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/", methods=["POST"])
+def login():
+    """
+    This is post request to home page
+    """
+    login = request.form.get("login")
+    password = request.form.get("password")    
+    # And then create user session
+    return render_template("home.html")
+
+
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    FLASK_DEBUG=1
+    TEMPLATES_AUTO_RELOAD = True
+    app.run(debug=True)
 
